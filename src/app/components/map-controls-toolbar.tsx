e0a41f168099da6508b8a125b8de9addceb0a9db
Peet Janse van Vuuren <peetjvv93@gmail.com>
2020-12-24 11:45:44 +0200

    map controls toolbar finished

--- src/app/components/map-controls-toolbar.tsx
+++ src/app/components/map-controls-toolbar.tsx
@@ -1,5 +1,42 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as FaSolidIcons from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { MyViewportProps } from '../../types/mapbox';
+
+const MapControlButton: React.FC<{
+  className?: string;
+  disabled?: boolean;
+  icon: IconDefinition;
+  iconStyle?: React.CSSProperties;
+  onClick: () => void;
+  value?: number;
+}> = props => {
+  const {
+    className = '',
+    disabled = false,
+    icon,
+    iconStyle = {},
+    onClick,
+    value,
+  } = props;
+
+  return (
+    <div
+      className={`map-control-button ${
+        disabled ? 'disabled' : ''
+      } ${className}`}
+      onClick={() => {
+        if (!disabled) {
+          onClick();
+        }
+      }}
+    >
+      <FontAwesomeIcon style={iconStyle} icon={icon} />
+      {value !== undefined ? <span>{value.toFixed(1)}</span> : null}
+    </div>
+  );
+};
 
 const MapControlsToolbar: React.FC<{
   viewport: MyViewportProps;
@@ -7,10 +44,55 @@ const MapControlsToolbar: React.FC<{
 }> = props => {
   const { viewport, setViewport } = props;
 
-  const showPitch = !!viewport['pitch'];
-  const showBearing = !!viewport['bearing'];
+  const transitionDuration = 500;
 
-  return <div className="map-controls-toolbar">Map controls here</div>;
+  return (
+    <div className="map-controls-toolbar">
+      <MapControlButton
+        disabled={viewport.zoom === 21}
+        icon={FaSolidIcons.faPlus}
+        onClick={() =>
+          setViewport({
+            ...viewport,
+            zoom: viewport.zoom + 1,
+            transitionDuration,
+          })
+        }
+      />
+      <MapControlButton
+        disabled={viewport.zoom === 1}
+        icon={FaSolidIcons.faMinus}
+        onClick={() =>
+          setViewport({
+            ...viewport,
+            zoom: viewport.zoom - 1,
+            transitionDuration,
+          })
+        }
+      />
+      {viewport['bearing'] !== undefined ? (
+        <MapControlButton
+          disabled={viewport.bearing === 0}
+          icon={FaSolidIcons.faLocationArrow}
+          iconStyle={{ transform: `rotate(${viewport.bearing - 45}deg)` }}
+          onClick={() =>
+            setViewport({ ...viewport, bearing: 0, transitionDuration })
+          }
+          value={viewport.bearing}
+        />
+      ) : null}
+      {viewport['pitch'] !== undefined ? (
+        <MapControlButton
+          disabled={viewport.pitch === 0}
+          icon={FaSolidIcons.faMagic}
+          onClick={() =>
+            setViewport({ ...viewport, pitch: 0, transitionDuration })
+          }
+          value={viewport.pitch}
+        />
+      ) : null}
+    </div>
+  );
 };
 
 export default MapControlsToolbar;
--- src/types/mapbox.ts
+++ src/types/mapbox.ts
@@ -2,5 +2,12 @@ import { ViewportProps } from 'react-map-gl';
 
 export type MyViewportProps = Pick<
   ViewportProps,
-  'width' | 'height' | 'latitude' | 'longitude' | 'zoom' | 'pitch' | 'bearing'
+  | 'width'
+  | 'height'
+  | 'latitude'
+  | 'longitude'
+  | 'zoom'
+  | 'pitch'
+  | 'bearing'
+  | 'transitionDuration'
 >;
